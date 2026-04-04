586df09cc0487fa0738e1373760a10475af217ee
tapiwa-creator <tapiwacomfort086@gmail.com>
2026-04-05 00:34:24 +0200

    changes

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -58,7 +58,7 @@ const Contact = () => {
       icon: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />,
     },
     {
-      label: 'Phone', value: '+236 781406806, +263 168 8458', href: 'tel:+236781406806',
+      label: 'Phone', value: '+236 781406806, +263 771688458', href: 'tel:+236781406806',
       bg: 'rgba(13,148,136,0.12)', iconColor: '#34d399',
       icon: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />,
     },
